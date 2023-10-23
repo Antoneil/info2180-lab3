e132369599b5b48a835ac280b4b067b061934614
Antoneil Singh <antoneilsingh@gmail.com>
2023-10-22 19:01:57 -0500

    Change style when hovered

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,3 @@
-// Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   // Get a reference to the game board
   var board = document.getElementById("board");
@@ -6,35 +5,37 @@ document.addEventListener("DOMContentLoaded", function () {
   // Create an array to hold the class names for each square
   var classNames = ["top-left", "top-center", "top-right", "middle-left", "middle-center", "middle-right", "bottom-left", "bottom-center", "bottom-right"];
 
-  // Loop through each square and add the appropriate class
-  for (var i = 0; i < board.children.length; i++) {
-    var square = board.children[i];
-    square.classList.add("square", classNames[i]);
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var board = document.getElementById("board");
-  var squares = board.getElementsByClassName("square");
+  // Initialize game state variables
   var currentPlayer = "X"; // Start with "X"
   var gameBoard = ["", "", "", "", "", "", "", "", ""]; // Initialize an empty game board
 
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", function () {
-      var square = this;
+  // Loop through each square and add the appropriate class, event listeners
+  for (var i = 0; i < board.children.length; i++) {
+    var square = board.children[i];
+    square.classList.add("square", classNames[i]);
 
-      // Check if the square is empty (not already marked)
-      if (square.textContent === "") {
+    square.addEventListener("click", function () {
+      if (this.textContent === "") {
         // Update the square with the current player (X or O)
-        square.textContent = currentPlayer;
-        square.classList.add(currentPlayer);
+        this.textContent = currentPlayer;
+        this.classList.add(currentPlayer);
 
         // Update the game state array
-        gameBoard[i] = currentPlayer;
+        gameBoard[classNames.indexOf(this.classList[1])] = currentPlayer;
 
         // Toggle the current player
         currentPlayer = currentPlayer === "X" ? "O" : "X";
       }
+    });
+
+    square.addEventListener("mouseover", function () {
+      // Add the 'hover' class when the mouse hovers over the square
+      this.classList.add("hover");
+    });
+
+    square.addEventListener("mouseout", function () {
+      // Remove the 'hover' class when the mouse leaves the square
+      this.classList.remove("hover");
     });
   }
 });
